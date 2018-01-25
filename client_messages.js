99d27ece58bcf150fd2eaea3a54f473e4549832b
Fabrice Dugas <fabby.dugas@gmail.com>
2018-01-24 19:08:36 -0500

    added basic client in javascript

---
+++ client_messages.js
@@ -0,0 +1,7 @@
+var exampleSocket = new WebSocket("ws://localhost:3000/cable");
+exampleSocket.onmessage = function (event) {
+  var data = JSON.parse(event.data)
+  if (data.type != "ping") console.log(data)
+}
+exampleSocket.send(JSON.stringify({ command: "subscribe", identifier: "{ \"channel\": \"ChatroomChannel\" }"}));
+exampleSocket.send(JSON.stringify({ command: "message",   identifier: "{ \"channel\": \"ChatroomChannel\" }", "data": "{ \"action\": \"message\", \"content\": \"salut\"}" }));
