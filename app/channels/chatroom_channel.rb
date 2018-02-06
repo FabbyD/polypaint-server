6d13f3c82f612e3a13f3bbe6b99f887843cc6159
Fabrice Dugas <fabby.dugas@gmail.com>
2018-02-05 23:23:24 -0500

    added timestamp to messages

--- app/channels/chatroom_channel.rb
+++ app/channels/chatroom_channel.rb
@@ -10,11 +10,12 @@ class ChatroomChannel < ApplicationCable::Channel
   def message(data)
     #message = Message.new(content: data["content"])
     #message.user = User.find_by(id: current_user)
-    #message.chatroom = Chatroom.find_by(id: 1) # FIXME take id from user
+    #message.chatroom = Chatroom.find_by(id: 1) # FIXME take id in session
     #if message.save
       ActionCable.server.broadcast 'chatroom_channel',
         message: data["content"], # FIXME message.content
         user: 'Pablo' # FIXME message.user
+        time: DateTime.now # FIXME message.date_created
     #else
     #  puts "Failed to save message in database: #{message.errors.full_messages}"
     #end
