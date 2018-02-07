f74a44e797ff802eb49555cbe7146dbef46cb3b9
Fabrice Dugas <fabby.dugas@gmail.com>
2018-02-07 09:57:32 -0500

    return user and timestamp

--- app/channels/chatroom_channel.rb
+++ app/channels/chatroom_channel.rb
@@ -8,16 +8,16 @@ class ChatroomChannel < ApplicationCable::Channel
   end
 
   def message(data)
-    #message = Message.new(content: data["content"])
-    #message.user = User.find_by(id: current_user)
-    #message.chatroom = Chatroom.find_by(id: 1) # FIXME take id in session
-    #if message.save
+    message = Message.new(content: data["content"])
+    message.user = User.find_by(id: current_user)
+    message.chatroom = Chatroom.find_by(id: 1) # FIXME take room from user
+    if message.save
       ActionCable.server.broadcast 'chatroom_channel',
-        message: data["content"], # FIXME message.content
-        user: 'Pablo' # FIXME message.user
-        time: DateTime.now # FIXME message.date_created
-    #else
-    #  puts "Failed to save message in database: #{message.errors.full_messages}"
-    #end
+        message: message.content,
+        user: message.user.name,
+        time: message.created_at
+    else
+      puts "Failed to save message in database: #{message.errors.full_messages}"
+    end
   end
 end
