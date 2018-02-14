47148ab0a04530950aca6b59ea840d446787ef2d
Fabrice Dugas <fabby.dugas@gmail.com>
2018-02-14 08:42:37 -0500

    removed client_messages.js from remote

--- client_messages.js
+++
@@ -1,7 +0,0 @@
-var exampleSocket = new WebSocket("ws://localhost:3000/cable?user_id=2");
-exampleSocket.onmessage = function (event) {
-  var data = JSON.parse(event.data)
-  if (data.type != "ping") console.log(data)
-}
-exampleSocket.send(JSON.stringify({ command: "subscribe", identifier: "{ \"channel\": \"ChatroomChannel\" }"}));
-exampleSocket.send(JSON.stringify({ command: "message",   identifier: "{ \"channel\": \"ChatroomChannel\" }", "data": "{ \"action\": \"message\", \"content\": \"salut\"}" }));
