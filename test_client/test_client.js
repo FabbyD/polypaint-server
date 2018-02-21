9eabe87036da4de6a0648274dfe8d96c5e8af9ac
Fabrice Dugas <fabrice.dugas@polymtl.ca>
2018-02-21 12:36:21 -0500

    test_client pour CanvasChannel

--- test_client/test_client.js
+++ test_client/test_client.js
@@ -1,7 +1,26 @@
-var exampleSocket = new WebSocket("ws://localhost:3000/cable?user_id=3");
+var exampleSocket = new WebSocket("ws://10.200.22.191:3000/cable?user_id=3");
+
+var CHANNEL = "CanvasChannel"
+var ROOM = "Patate"
+
+function send(cmd, channel, room, data) {
+  var payload = {
+    command: cmd,
+    identifier: JSON.stringify({ channel: channel, room: room })
+  }
+  if (data) {
+    payload["data"] = data
+  }
+  exampleSocket.send(JSON.stringify(payload))
+}
 
 function send_test() {
-    console.log('Sending test.')
+  console.log('Sending test.')
+  var data = JSON.stringify({
+    action: "draw",
+    content: "beau content"
+  })
+  send("message", CHANNEL, ROOM, data)
 }
 
 exampleSocket.onmessage = function (event) {
@@ -13,10 +32,7 @@ exampleSocket.onmessage = function (event) {
 }
 exampleSocket.onopen = function(event) {
   console.log('Connexion opened.')
-  exampleSocket.send(JSON.stringify({
-    command: "subscribe",
-    identifier: JSON.stringify({ channel: "CanvasChannel", room: "patate" })
-  }))
+  send("subscribe", CHANNEL, ROOM)
 };
 exampleSocket.onclose = function(event) {
     console.log('Connexion closed.')
