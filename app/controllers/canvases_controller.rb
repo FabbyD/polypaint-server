df13d2411664705b672b38bb7fec450bf3760317
Fabrice Dugas <fabby.dugas@gmail.com>
2018-03-31 17:27:23 -0400

    added canvas thumbnails + eager load layers when pulling canvas

--- app/controllers/canvases_controller.rb
+++ app/controllers/canvases_controller.rb
@@ -1,10 +1,11 @@
 class CanvasesController < ApplicationController
   def show
-    canvas = Canvas.find_by(id: params[:id])
+    canvas = canvasSelect
+      .includes(layers: [:strokes, :canvas_images, :textboxes])
+      .find(params[:id])
     if canvas
       jcanvas = canvas.as_json()
-      layers = canvas.layers
-      jcanvas["layers"] = layers.map do |layer|
+      jcanvas["layers"] = canvas.layers.map do |layer|
         jlayer = layer.as_json()
         jlayer["strokes"] = layer.strokes
         jlayer["images"] = layer.canvas_images
@@ -12,25 +13,21 @@ class CanvasesController < ApplicationController
         jlayer
       end
       render status: :ok,
-        json: { canvas: jcanvas }.to_json
+        json: { canvas: jcanvas }
     else
       render status: :not_found
     end
   end
 
   def index
-    canvases = Canvas
-      .select('canvases.id, canvases.name, canvases.description, canvases.private, canvases.protected, canvases.width, canvases.height, users.name as user_name, canvases.created_at, canvases.updated_at')
-      .joins(:user)
+    canvases = canvasSelect
       .where(private: false)
     render status: :ok,
       json: { canvases: canvases }.to_json
   end
 
   def indexByUser
-    canvases = Canvas
-      .select('canvases.id, canvases.name, canvases.description, canvases.private, canvases.protected, canvases.width, canvases.height, users.name as user_name, canvases.created_at, canvases.updated_at')
-      .joins(:user)
+    canvases = canvasSelect
       .where('users.name = ?', params[:user_name])
 
     if params[:current_user] != params[:user_name]
@@ -48,5 +45,14 @@ class CanvasesController < ApplicationController
     else
 			head :unauthorized
     end
+  end
+
+  private
+
+  def canvasSelect
+    Canvas.select('canvases.id, canvases.name, canvases.description, canvases.thumbnail,
+                  canvases.private, canvases.protected, canvases.width, canvases.height,
+                  users.name as user_name, canvases.created_at, canvases.updated_at')
+      .joins(:user)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180331183609) do
+ActiveRecord::Schema.define(version: 20180331204454) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20180331183609) do
     t.boolean "private", default: true
     t.boolean "protected", default: false
     t.string "password_digest", default: ""
+    t.string "thumbnail"
     t.index ["user_id"], name: "index_canvases_on_user_id"
   end
 
