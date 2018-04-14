57d66a0c9debf137ef91ae5dc562f65727617de5
Fabrice Dugas <fabby.dugas@gmail.com>
2018-04-13 21:54:03 -0400

    fix populate_db templates

--- lib/populate_database.rb
+++ lib/populate_database.rb
@@ -29,6 +29,6 @@ pixel_canvas.save
 p pixel_canvas
 
 # Initialize default templates
-p Template.create(url: "https://s3.ca-central-1.amazonaws.com/polypaint-pro-staging/templates/defaults/timon-pumbaa.jpg", width: 700, height: 980)
+p Template.create(url: "https://s3.ca-central-1.amazonaws.com/polypaint-pro-staging/templates/defaults/timon-pumbaa.png", width: 700, height: 980)
 p Template.create(url: "https://s3.ca-central-1.amazonaws.com/polypaint-pro-staging/templates/defaults/meme.png", width: 640, height: 400)
 p Template.create(url: "https://s3.ca-central-1.amazonaws.com/polypaint-pro-staging/templates/defaults/fade.png", width: 640, height: 400)
